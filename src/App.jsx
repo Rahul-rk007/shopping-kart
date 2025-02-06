da7bb9a3321078346077868062df0842aa97097a
Rahul <rp.rahul02@gmail.com>
2025-02-06 17:07:27 +0530

    Completed product detail page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Shop from "./components/Shop/Shop";
+import ProductDetails from "./components/Product-Details/ProductDetails";
 
 function App() {
   return (
     <>
       {/* <Home /> */}
-      <Shop />
+      {/* <Shop /> */}
+      <ProductDetails />
     </>
   );
 }
