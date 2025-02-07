3a95819bcff975892f5d1da18d76228ed29cd7ea
Riddhi002 <prajapati.riddhi002@gmail.com>
2025-02-07 15:09:58 +0530

    create cart page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import Shop from "./components/Shop/Shop";
 import ProductDetails from "./components/Product-Details/ProductDetails";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <>
       {/* <Home /> */}
       {/* <Shop /> */}
-      <ProductDetails />
+      {/* <ProductDetails /> */}
+      <Cart />
     </>
   );
 }
