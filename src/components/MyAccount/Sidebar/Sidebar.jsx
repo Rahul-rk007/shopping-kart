108437120b45f6910dbfab9c8aeb61aec58b1447
Rahul <rp.rahul02@gmail.com>
2025-03-05 15:01:41 +0530

    update sidebar design

--- src/components/MyAccount/Sidebar/Sidebar.jsx
+++ src/components/MyAccount/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom"; // Import NavLink and useLocation
 import "./Sidebar.css";
 import Profile from "../../../assets/icon/profile.webp";
 import MyOrder from "../../../assets/icon/myorder.png";
@@ -8,48 +8,83 @@ import Wishlist from "../../../assets/icon/wishlist.png";
 import ChangePassword from "../../../assets/icon/changepassword.png";
 
 const Sidebar = () => {
+  const location = useLocation(); // Get the current location
+
+  // Function to determine if the current path matches the link's path
+  const getActiveClass = (path) => {
+    return location.pathname === path ? "active" : "";
+  };
+
   return (
     <div className="myaccount-sidebar">
       <ul className="myaccount-sidebar-container">
-        <li className="myaccount-list myaccount-list-link">
-          <img className="myaccount-list-img" src={Profile} />
-          <Link className="myaccount-link" to="/myaccount/profile">
+        <li
+          className={`myaccount-list myaccount-list-link ${getActiveClass(
+            "/myaccount/profile"
+          )}`}
+        >
+          <NavLink className="myaccount-link" to="/myaccount/profile">
+            <img className="myaccount-list-img" src={Profile} alt="Profile" />
             Profile
-          </Link>
+          </NavLink>
         </li>
         <hr />
-        <li className="myaccount-list myaccount-list-link">
-          <img className="myaccount-list-img" src={MyOrder} />
-          <Link className="myaccount-link" to="/myaccount/myorders">
+        <li
+          className={`myaccount-list myaccount-list-link ${getActiveClass(
+            "/myaccount/myorders"
+          )}`}
+        >
+          <NavLink className="myaccount-link" to="/myaccount/myorders">
+            <img className="myaccount-list-img" src={MyOrder} alt="My Orders" />
             My Orders
-          </Link>
+          </NavLink>
         </li>
         <hr />
-        <li className="myaccount-list myaccount-list-link">
-          <img className="myaccount-list-img" src={Address} />
-          <Link className="myaccount-link" to="/myaccount/address">
+        <li
+          className={`myaccount-list myaccount-list-link ${getActiveClass(
+            "/myaccount/address"
+          )}`}
+        >
+          <NavLink className="myaccount-link" to="/myaccount/address">
+            <img className="myaccount-list-img" src={Address} alt="Address" />
             Address
-          </Link>
+          </NavLink>
         </li>
         <hr />
-        <li className="myaccount-list myaccount-list-link">
-          <img className="myaccount-list-img" src={Wishlist} />
-          <Link className="myaccount-link" to="/myaccount/wishlist">
+        <li
+          className={`myaccount-list myaccount-list-link ${getActiveClass(
+            "/myaccount/wishlist"
+          )}`}
+        >
+          <NavLink className="myaccount-link" to="/myaccount/wishlist">
+            <img className="myaccount-list-img" src={Wishlist} alt="Wishlist" />
             Wishlist
-          </Link>
+          </NavLink>
         </li>
         <hr />
-        <li className="myaccount-list myaccount-list-link">
-          <img className="myaccount-list-img" src={ChangePassword} />
-          <Link className="myaccount-link" to="/myaccount/changepassword">
+        <li
+          className={`myaccount-list myaccount-list-link ${getActiveClass(
+            "/myaccount/changepassword"
+          )}`}
+        >
+          <NavLink className="myaccount-link" to="/myaccount/changepassword">
+            <img
+              className="myaccount-list-img"
+              src={ChangePassword}
+              alt="Change Password"
+            />
             Change Password
-          </Link>
+          </NavLink>
         </li>
         <hr />
-        <li className="myaccount-list myaccount-logout-btn">
-          <Link className="btn btn-danger" to="/logout">
+        <li
+          className={`myaccount-list myaccount-logout-btn ${getActiveClass(
+            "/logout"
+          )}`}
+        >
+          <NavLink className="btn btn-danger" to="/logout">
             Logout
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </div>
